51db9f1e4ab9c6f94c2e77236d8718b12f4b3096
diana lopez <ctcuser128@ip-172-31-46-179.us-east-2.compute.internal>
2023-11-13 02:25:02 +0000

    adding a python program

---
+++ dl11.py
@@ -0,0 +1,11 @@
+#Diana Lopez
+#CSC321
+#Lab11
+
+def square(num):
+    return (num*num)
+
+number = int (input("Enter a number: "))
+
+print("Your number squared is {1}, Take it one day at a time!".format(number, square(number)))
+
